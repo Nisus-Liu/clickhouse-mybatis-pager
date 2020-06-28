6325178f0578a15c1937e8e7211f18aa3da23b38
dafei <dafei@ibantang.com>
2020-06-28 16:10:47 +0800

    ???

---
+++ src/main/java/MyPage.java
@@ -0,0 +1,255 @@
+import cn.hutool.db.Page;
+import com.baomidou.mybatisplus.core.metadata.OrderItem;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 定制的分页对象
+ *
+ * 页号从 1 开始计.
+ * 记录起始位置以 0 开始计.
+ * @see {@link Page}
+ * @author dafei
+ * @version 0.1
+ * @date 2019/11/22 13:28
+ */
+public class MyPage<T> {
+
+    public static final int DEFAULT_PAGE_SIZE = 20;
+
+    /**
+     * 页码
+     */
+    private int pageNumber;
+    /**
+     * 每页结果数
+     */
+    private int pageSize;
+    /**
+     * 排序
+     */
+    private List<OrderItem> orders;
+
+    /**
+     * 是否进行 count 查询
+     */
+    private boolean isSearchCount = true;
+
+    /**
+     * 查询数据列表
+     */
+    private List<T> records = Collections.emptyList();
+
+    /**
+     * 总数
+     */
+    private long total = 0;
+
+    // ---------------------------------------------------------- Constructor start
+
+    public MyPage() {}
+
+    /**
+     * 构造
+     *
+     * @param pageNumber 页码
+     * @param pageSize   每页结果数
+     */
+    public MyPage(int pageNumber, int pageSize) {
+        this(pageNumber, pageSize, true);
+    }
+
+    public MyPage(int pageNumber, int pageSize, boolean isSearchCount) {
+        this.pageNumber = pageNumber < 0 ? 0 : pageNumber;
+        this.pageSize = pageSize <= 0 ? DEFAULT_PAGE_SIZE : pageSize;
+        this.isSearchCount = isSearchCount;
+    }
+
+    /**
+     * 构造
+     *
+     * @param pageNumber 页码
+     * @param numPerPage 每页结果数
+     * @param order      排序对象
+     */
+    public MyPage(int pageNumber, int numPerPage, OrderItem order) {
+        this(pageNumber, numPerPage);
+        if (this.orders == null) {
+            this.orders = new ArrayList<OrderItem>();
+        }
+        this.orders.add(order);
+    }
+    // ---------------------------------------------------------- Constructor start
+
+    // ---------------------------------------------------------- Getters and Setters start
+
+    /**
+     * @return 页码
+     */
+    public int getPageNumber() {
+        return pageNumber;
+    }
+
+    /**
+     * 设置页码
+     *
+     * @param pageNumber 页码
+     * @return
+     */
+    public MyPage<T> setPageNumber(int pageNumber) {
+        this.pageNumber = pageNumber < 0 ? 0 : pageNumber;
+        return this;
+    }
+
+    /**
+     * @return 每页结果数
+     */
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    /**
+     * 设置每页结果数
+     *
+     * @param pageSize 每页结果数
+     * @return
+     */
+    public MyPage<T> setPageSize(int pageSize) {
+        this.pageSize = pageSize <= 0 ? DEFAULT_PAGE_SIZE : pageSize;
+        return this;
+    }
+
+    public long getTotal() {
+        return this.total;
+    }
+
+    public MyPage<T> setTotal(long total) {
+        this.total = total;
+        return this;
+    }
+
+    public MyPage<T> setRecords(List<T> records) {
+        this.records = records;
+        return this;
+    }
+
+    public List<T> getRecords() {
+        return this.records;
+    }
+
+
+    /**
+     * @return 排序
+     */
+    public List<OrderItem> getOrders() {
+        return this.orders;
+    }
+
+    /**
+     * 设置排序
+     *
+     * @param orders 排序
+     * @return
+     */
+    public MyPage<T> setOrder(OrderItem... orders) {
+        this.orders = Arrays.asList(orders);
+        return this;
+    }
+
+    /**
+     * 设置排序
+     *
+     * @param orders 排序
+     */
+    public void addOrder(OrderItem... orders) {
+        if (null != this.orders) {
+            this.orders.addAll(Arrays.asList(orders));
+        }
+        this.orders = Arrays.asList(orders);
+    }
+
+    public boolean isSearchCount() {
+        if (total < 0) {
+            return false;
+        }
+        return isSearchCount;
+    }
+
+    public MyPage<T> setSearchCount(boolean isSearchCount) {
+        this.isSearchCount = isSearchCount;
+        return this;
+    }
+
+    // ---------------------------------------------------------- Getters and Setters end
+
+    /**
+     * @return 开始位置
+     */
+    public int getStartPosition() {
+        return getStartEnd()[0];
+    }
+
+    /**
+     * @return 结束位置
+     */
+    public int getEndPosition() {
+        return getStartEnd()[1];
+    }
+
+    /**
+     * 开始位置和结束位置<br>
+     * 例如：<br>
+     * 页码：1，每页10 =》 [0, 10]<br>
+     * 页码：2，每页10 =》 [10, 20]<br>
+     * 。。。<br>
+     *
+     * @return 第一个数为开始位置，第二个数为结束位置
+     */
+    public int[] getStartEnd() {
+        return MyPage.transToStartEnd(pageNumber, pageSize);
+    }
+
+    @Override
+    public String toString() {
+        return "Page [page=" + pageNumber + ", pageSize=" + pageSize + ", order=" + orders +
+                ", total=" + total + ", records.size=" + (records==null?null:records.size()) +
+                "]";
+    }
+
+
+    /**
+     * 将页数和每页条目数转换为开始位置和结束位置<br>
+     * 此方法用于不包括结束位置的分页方法<br>
+     * 例如：
+     *
+     * <pre>
+     * 页码：1，每页10 =》 [0, 10]
+     * 页码：2，每页10 =》 [10, 20]
+     * ……
+     * </pre>
+     *
+     * @param pageNo 页码（从1计数）
+     * @param pageSize 每页条目数
+     * @return 第一个数为开始位置，第二个数为结束位置
+     */
+    public static int[] transToStartEnd(int pageNo, int pageSize) {
+        if (pageNo < 1) {
+            pageNo = 1;
+        }
+
+        if (pageSize < 1) {
+            pageSize = 0;
+        }
+
+        final int start = (pageNo - 1) * pageSize;
+        if (pageSize < 1) {
+            pageSize = 0;
+        }
+        final int end = start + pageSize;
+
+        return new int[] { start, end };
+    }
+}
---
+++ src/main/java/MyPaginationInterceptor.java
@@ -0,0 +1,207 @@
+package com.sqkb.bigdata.config.mybatis;
+
+import com.baomidou.mybatisplus.core.MybatisDefaultParameterHandler;
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.sqkb.bigdata.common.bean.vo.MyPage;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.mapping.SqlCommandType;
+import org.apache.ibatis.mapping.StatementType;
+import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.reflection.DefaultReflectorFactory;
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.reflection.SystemMetaObject;
+import org.apache.ibatis.scripting.defaults.DefaultParameterHandler;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Field;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+/**
+ * 针对 clickhouse 等 mybatis plus 不支持的的mybatis分页拦截器
+ *
+ * mybatis-plus 不支持clickhouse的分页sql处理
+ *
+ * 参照: com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor
+ *
+ * @author dafei
+ * @version 0.1
+ * @date 2019/11/22 12:14
+ */
+@Slf4j
+@Setter
+@Component
+@Accessors(chain = true)
+@Intercepts({@Signature(type = StatementHandler.class, method = "prepare", args = {Connection.class, Integer.class})})
+public class MyPaginationInterceptor implements Interceptor {
+
+    public static final Set<String> SUPPORT_DIALECTTYPES = new HashSet<String>(){{
+        add("clickhouse");
+    }};
+    /**
+     * 单页限制 500 条，小于 0 如 -1 不受限制
+     */
+    private final int limit = 500;
+
+    private String dialectType;
+
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+        long st = System.currentTimeMillis();
+        StatementHandler statementHandler = (StatementHandler) invocation.getTarget();
+        //通过MetaObject优雅访问对象的属性，这里是访问statementHandler的属性;：MetaObject是Mybatis提供的一个用于方便、
+        //优雅访问对象属性的对象，通过它可以简化代码、不需要try/catch各种reflect异常，同时它支持对JavaBean、Collection、Map三种类型对象的操作。
+        MetaObject metaObject = MetaObject
+                .forObject(statementHandler, SystemMetaObject.DEFAULT_OBJECT_FACTORY, SystemMetaObject.DEFAULT_OBJECT_WRAPPER_FACTORY,
+                        new DefaultReflectorFactory());
+        //先拦截到RoutingStatementHandler，里面有个StatementHandler类型的delegate变量，其实现类是BaseStatementHandler，然后就到BaseStatementHandler的成员变量mappedStatement
+        MappedStatement mappedStatement = (MappedStatement) metaObject.getValue("delegate.mappedStatement");
+        //id为执行的mapper方法的全路径名，如com.uv.dao.UserMapper.insertUser
+        String id = mappedStatement.getId();
+        //sql语句类型 select、delete、insert、update
+        // String sqlCommandType = mappedStatement.getSqlCommandType().toString();
+        if (!isSupported(mappedStatement)) {
+            return invocation.proceed();
+        }
+
+
+
+        //数据库连接信息
+//        Configuration configuration = mappedStatement.getConfiguration();
+//        ComboPooledDataSource dataSource = (ComboPooledDataSource)configuration.getEnvironment().getDataSource();
+//        dataSource.getJdbcUrl();
+
+        BoundSql boundSql = statementHandler.getBoundSql();
+        Object paramObj = boundSql.getParameterObject();
+        // 判断参数里是否有page对象
+        MyPage<?> page = null;
+        if (paramObj instanceof MyPage) {
+            page = (MyPage<?>) paramObj;
+        } else if (paramObj instanceof Map) {
+            for (Object arg : ((Map<?, ?>) paramObj).values()) {
+                if (arg instanceof MyPage) {
+                    page = (MyPage<?>) arg;
+                    break;
+                }
+            }
+        }
+
+        /*
+         * 不需要分页的场合，如果 size 小于 0 返回结果集
+         */
+        if (null == page || page.getPageSize() < 0) {
+            return invocation.proceed();
+        }
+
+        log.debug("----sqlId----"+id);
+
+
+        /*
+         * 处理单页条数限制
+         */
+        if (limit <= page.getPageSize()) {
+            page.setPageSize(limit);
+        }
+
+        //获取到原始sql语句
+        String originalSql = boundSql.getSql();
+        Connection connection = (Connection) invocation.getArgs()[0];
+
+        // 查询总记录数
+        if (page.isSearchCount()) {
+            String countSql = getCountSql(originalSql);
+            long t0 = System.currentTimeMillis();
+            this.queryTotal(countSql, mappedStatement, boundSql, connection, page);
+            log.debug("get total, cost time: {}ms", System.currentTimeMillis() - t0);
+            if (page.getTotal() <= 0) {
+                return null;
+            }
+        }
+
+
+        // 分页查询
+        //通过反射修改sql语句
+        String pageSql = this.buildPaginationSql(originalSql, page);
+        Field field = boundSql.getClass().getDeclaredField("sql");
+        field.setAccessible(true);
+        field.set(boundSql, pageSql);
+        log.debug("original sql: {}, after paging: {}", originalSql, pageSql);
+
+        long t0 = System.currentTimeMillis();
+        Object ret = invocation.proceed();
+        log.debug("page query, cost time: {}ms", System.currentTimeMillis() - t0);
+        log.debug("total cost time: {}ms", System.currentTimeMillis() - st);
+        return ret;
+    }
+
+    @Override
+    public Object plugin(Object o) {
+        return Plugin.wrap(o, this);
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+        // ? 没走这里 ?
+        String dialectType = properties.getProperty("dialectType");
+        log.debug("dialectType: {}",dialectType);
+        if (StringUtils.isNotEmpty(dialectType)) {
+            this.dialectType = dialectType;
+        }
+    }
+
+    private boolean isSupported(MappedStatement mappedStatement) {
+        if (SqlCommandType.SELECT != mappedStatement.getSqlCommandType()
+                || StatementType.CALLABLE == mappedStatement.getStatementType()
+                /*|| !SUPPORT_DIALECTTYPES.contains(dialectType)*/) {
+            return false;
+        }
+        return true;
+    }
+
+
+    private String buildPaginationSql(String originalSql, MyPage<?> page) {
+        String sql = originalSql + " LIMIT " + page.getStartPosition() + StringPool.COMMA + page.getPageSize();
+        return sql;
+    }
+
+    private String getCountSql(String originalSql) {
+        return String.format("SELECT COUNT(1) FROM ( %s ) TOTAL", originalSql);
+    }
+
+    private void queryTotal(String sql, MappedStatement mappedStatement, BoundSql boundSql, Connection connection, MyPage<?> page) {
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            // 借助 mybatis plus 帮我设置参数
+            DefaultParameterHandler parameterHandler = new MybatisDefaultParameterHandler(mappedStatement, boundSql.getParameterObject(), boundSql);
+            parameterHandler.setParameters(statement);
+            long total = 0;
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    total = resultSet.getLong(1);
+                }
+            }
+            page.setTotal(total);
+            // /*
+            //  * 溢出总页数，设置第一页
+            //  */
+            // long pages = page.getPages();
+            // if (overflowCurrent && page.getCurrent() > pages) {
+            //     // 设置为第一条
+            //     page.setCurrent(1);
+            // }
+
+        } catch (Exception e) {
+            throw new RuntimeException("ClickhousePaginationInterceptor#queryTotal error, sql: "+sql, e);
+        }
+    }
+}
